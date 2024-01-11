ef18e52f4b5352d089ecee1574a3507c40346d40
horlali <gideon.ahiadzi@gmail.com>
2024-01-11 01:36:08 +0000

    Update config and dependencies, add Docker

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -8,8 +8,9 @@ cd $(dirname $0)/..
 NAME="GPA-Calculator"
 PROJECT_DIR="${PWD}/gpa_calculator"
 MODULE_NAME="client/app.py"
+HOST="0.0.0.0"
 PORT=8501
 
 echo "Starting ${NAME} client..."
 
-streamlit run ${PROJECT_DIR}/${MODULE_NAME} --server.port ${PORT}
+streamlit run ${PROJECT_DIR}/${MODULE_NAME} --server.port ${PORT} --server.address ${HOST}
